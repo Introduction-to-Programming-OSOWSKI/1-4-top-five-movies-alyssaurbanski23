9957c079a126977ba31acdc20fb3d2c52db8c12f
alyssaurbanski23 <831554@mystma.org>
2020-12-18 21:50:52 +0000

    Favorite Moives try 1

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-#WRITE YOUR CODE IN THIS FILE
+def wordSmash (wordOne, wordTwo, wordThree, wordFour, wordFive):
+    return wordOne + wordTwo + wordThree + wordFour + wordFive
+
+print (wordSmash("The nutcracker", "If I Stay", "Beauty and the Beast", "The Nightmare Before Christmas", "Corpse Bride"))
